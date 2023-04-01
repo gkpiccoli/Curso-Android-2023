c733161002addd25f96bacffc4856915faec8de9
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-04-01 18:20:23 -0300

    SharedPreferences - Limpar os dados salvos.

--- AppListaCurso/app/src/main/java/devandroid/gkp/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/gkp/applistacurso/view/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences_LimparDados
         preferences = getSharedPreferences(NOME_PREFERENCES,0);
         listaInscricao = preferences.edit();
 
