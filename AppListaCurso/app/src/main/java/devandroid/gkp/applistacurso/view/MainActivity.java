b283ef1d9267a42e35f38c4f99b3a7d1bd55498b
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-04-06 02:13:08 -0300

    Layout Refatorado para o LinearLayout

--- AppListaCurso/app/src/main/java/devandroid/gkp/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/gkp/applistacurso/view/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
         preferences = getSharedPreferences(NOME_PREFERENCES, 0);
         listaInscricao = preferences.edit();
